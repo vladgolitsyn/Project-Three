8d622c781640df844ef27be074cf2de72ac4aef3
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-06-14 18:08:23 -0400

    used routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-
 const routes = require("./routes");
 const app = express();
+const passport = require("passport");
+const flash = require("connect-flash");
+
+const session = require("express-session");
 const PORT = process.env.PORT || 3001;
 let db = require("./models");
-
+require("./config/passport")(passport);
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,16 +15,33 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+
+app.use(passport.initialize());
+app.use(
+  session({
+    secret: "cool"
+  })
+);
+
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  if (req.isAuthenticated) {
+    res.locals.isAuthenticated = req.isAuthenticated();
+  }
+  next();
+});
+
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
+  next();
+});
+
 app.use(routes);
-
-// Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBook");
-
-// // Start the API server
-// app.listen(PORT, function() {
-//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-// });
 db.sequelize.sync().then(function() {
   console.log("started!!!");
 
