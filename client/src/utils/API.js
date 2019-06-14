81d2da9e1363170921ca2b3b547e9384964041df
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-06-14 18:07:19 -0400

    Created user data from registration api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,7 @@
 import axios from "axios";
 
 export default {
-  saveUser: function(userSaveChoice) {
-    return axios.post("/api/users", userSaveChoice);
-  },
-
-  getUsers: function() {
-    return axios.get("/api/users");
-  },
-
-  getUser: function(id) {
-    return axios.get("/api/users/" + id);
-  },
-
-  deleteUser: function(id) {
-    return axios.delete("/api/users/" + id);
+  saveUsers: function(newUser) {
+    return axios.post("api/users/register", newUser);
   }
 };
