e95db4d2637486d32c998e647cf03647652cf686
szewah <swgrleong@gmail.com>
2019-06-24 17:28:17 -0400

    semi-fixed chat

--- client/src/pages/chat/Chat.js
+++ client/src/pages/chat/Chat.js
@@ -3,22 +3,29 @@ import {Container, Comment, Form} from 'semantic-ui-react';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import "./style.css";
+import Footer from "../../components/Footer";
+import io from "socket.io-client";
 
 
 class Chat extends Component{
-        state = {
-            message: '',
-            messages: [],
-        };
+
+    socket = null;
     
+    state = {
+        message: '',
+        messages: [],
+    };
+
     componentDidMount() {
-        console.log(this.props);
-        const { socket } = this.props;
-        
-        socket.on('connect', function() {
-            console.log('connected')
+       this.socket = io.connect('http://localhost:3001')
+        // console.log(this.props);
+        // const {socket} = this.props;
+        // console.log(socket);
+        this.socket.on('connect', (data) =>{
+            console.log('This is the chat socket connection ', data)
         });
-        socket.on('new-message', message => {
+
+        this.socket.on('new-message', message => {
             const { messages } = this.state;
             const udpatedMessages = [...messages, message];
             this.setState({ messages: udpatedMessages });
@@ -26,19 +33,21 @@ class Chat extends Component{
     }
 
 
-    handleChange = e => {
-        this.setState({[e.target.name]: e.target.value})
-    }
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+    };
 
     sendMessage = e => {
-        const { socket } = this.props;
+        // const { socket } = this.props;
         const {message} = this.state;
         this.setState({message})
         const user = this.props.auth.user.name;
-        socket.emit('new-message', `${user}: ${message}`)
+        this.socket.emit('new-message', `${user}: ${message}`)
         console.log(this.state.message);
-    }
-
+    };
 
     render() {
         return (
@@ -47,15 +56,12 @@ class Chat extends Component{
                     <p className="chat-header-p">Event Name Chat Group</p>
                 </div>
 
-                <Container>
+                <Container style={{marginTop: '30px'}}>
                 <div className="event-chat-area">
                     {this.state.messages.map((message, index) => {
                         return ( 
                             <Comment key={index}>
-                                <Comment.Content>     
-                                    <Comment.Metadata>
-                                    <div>Five days ago</div>
-                                    </Comment.Metadata>
+                                <Comment.Content style={{margin: '10px'}}>     
                                     <Comment.Text>{message}</Comment.Text>
                                 </Comment.Content>
                             </Comment>
@@ -70,9 +76,9 @@ class Chat extends Component{
                         placeholder='Message'
                         name='message'
                         value={this.state.message}
-                        onChange={this.handleChange}
                         className="event-chat-input"
                         style={{height: '50px'}} 
+                        onChange={this.handleChange}
                         />
                     <Form.Button 
                         content='+' 
@@ -81,11 +87,11 @@ class Chat extends Component{
                     />
                 </Form>
                 </Container>
-
+                <Footer style={{bottom: '0'}}/>
             </div>
-        )
+        );
     }
-}
+};
 
 // export default Chat;
 Chat.propTypes = {
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -3,38 +3,39 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import "./style.css";
 import { Redirect } from 'react-router-dom'
-import { Button, Card, Container, Header, Icon, Image } from "semantic-ui-react";
+import { Button, Card, Container, Image } from "semantic-ui-react";
 import Footer from "../../components/Footer";
 import Profile from "../../images/profile.png"
 
 class Dashboard extends Component {
+
   socket = null;
 
   state = {
     redirect: false
-  }
+  };
 
   componentDidMount() {
     //connecting the socket to the server once the component has mounted
     const {socket} = this.props;
-    socket.on('connect', () => {
-        console.log('Dashbord socket is connected')
+    console.log("this is the dashbord prop " + this.props)
+    socket.on('connect', data => {
+        console.log('Connected', data)
     });
   }
-
   
   onClick = () => {
     console.log(this.props)
     const name = this.props.auth.user.name
     this.setRedirect();
-    //on clicking the group icon, we join a "room"
+    // on clicking the group icon, we join a "room"
     const {socket} = this.props;
     socket.emit("join", name)
   };
 
   setRedirect = () => {
     this.setState({redirect: true})
-  }
+  };
 
   render() {
     if(this.state.redirect) {
@@ -74,7 +75,7 @@ class Dashboard extends Component {
               <Card.Content style={{textAlign:'center'}}>EVENT LIST</Card.Content>
               <br/>
               <br/>
-              <Card.Description>
+              <Card.Description style={{width: '40%', margin: 'auto'}}>
                 <Button className="eventGroup" onClick={this.onClick}>
                 Start chatting  
                 </Button>
@@ -86,7 +87,7 @@ class Dashboard extends Component {
       </div>
     );
   }
-}
+};
 
 Dashboard.propTypes = {
   auth: PropTypes.object.isRequired
