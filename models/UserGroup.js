ffb8aed3ac5c078a4a255a1d48de144c2d2915e8
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-06-24 18:49:16 -0400

    Changed the name

--- models/UserGroup.js
+++ models/UserGroup.js
@@ -1,5 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
-  const UserGroup = sequelize.define("UserGroup", {});
-
+  const UserGroup = sequelize.define("UserGroup", {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "id"
+      }
+    },
+    groupchatId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "GroupChat",
+        key: "id"
+      }
+    }
+  });
   return UserGroup;
 };
